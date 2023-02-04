268260ea320d13d6cbd66092257191e9c94641cd
Karoline <shiamosan@gmail.com>
2023-02-04 19:06:23 -0300

    Estrutura condicional "se"

---
+++ EstruturaCondicionalSe/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        int media = 10;
+        if(media >= 7){
+            if(media == 10) {
+                System.out.println("Parabéns! Você teve média perfeita");
+            } else {
+                System.out.println("Aprovado");
+            }
+        } else {
+            System.out.println("Reprovado");
+        }
+    }
+}
+
+/* OPERADORES LÓGICOS
+ * E = &&
+ * OU = ||
+ * NEGAÇÃO = !
+ */
