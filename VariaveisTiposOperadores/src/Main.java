d822e5e813ac1e98633315e49b272c45705603cb
Karoline <shiamosan@gmail.com>
2023-02-04 18:53:03 -0300

    Variaveis, tipos e operadores

---
+++ VariaveisTiposOperadores/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int idade = 10;
+        float cotacaoDolar = 5.0f;
+        double cotacaoEuro = 6.0d; //sempre devemos colocar a inicial do tipo após o valor da variável sendo ela double ou float
+        char genero = 'M'; //Apenas uma letra 
+        byte placar = 0; //numero inteiro
+        boolean estaCadastrado = false;
+
+        String nome = "Esse é um texto de uma variavel String.";
+
+        /*Operadores
+        +
+        -
+        /
+        *
+        %
+        */
+
+    }
+}
