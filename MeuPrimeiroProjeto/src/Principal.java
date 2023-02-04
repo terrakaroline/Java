b71d11949bcda0ffa080c95315fc8e0fc910633a
Karoline <shiamosan@gmail.com>
2023-02-04 18:21:05 -0300

    att

--- MeuPrimeiroProjeto/src/Principal.java
+++ MeuPrimeiroProjeto/src/Principal.java
@@ -3,4 +3,8 @@ public class Principal {
         System.out.println("Olá Mundo!");
         System.out.println("Meu segundo comando de saída");
     }
+    //Comentário de linha
+    /* Comentário de 
+    bloco */
+    /** Comentário de documentação, tudo que é digitado dentro dele pode ser usado por um app de terceiros para gerar a documentação */
 }
